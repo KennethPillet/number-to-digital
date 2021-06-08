102161488f9e6ebc6a13df309c7dd4bfb06c20b4
Kenneth Pillet <kennethpillet@hotmail.fr>
2021-06-08 11:15:12 +0200

    modify function

--- display.js
+++ display.js
@@ -1,36 +1,39 @@
 function convertToDigitalFormat(args){
     let numberArray = []
 
-    if(args.includes('0')){ 
-        numberArray.push(' _\n| |\n|_|')
+    for (const arg of args) {
+        if(arg ==  0){ 
+            numberArray.push(' _\n| |\n|_|')
+        }
+        if(arg ==  1){ 
+            numberArray.push('\n  |\n  |') 
+        }
+        if(arg == 2){ 
+            numberArray.push(' _\n _|\n|_ ')
+        }
+        if(arg == 3){ 
+            numberArray.push('_  \n_|\n_|')
+        }
+        if(arg == 4){ 
+            numberArray.push('  \n|_|\n  |')
+        }
+        if(arg == 5){ 
+            numberArray.push(' _\n|_ \n _|')
+        }
+        if(arg == 6){ 
+            numberArray.push(' _ \n|_ \n|_|')
+        }
+        if(arg == 7){ 
+            numberArray.push(' _\n  |\n  |')
+        }
+        if(arg == 8){ 
+            numberArray.push(' _\n|_|\n|_|')
+        }
+        if(arg == 9){ 
+            numberArray.push(' _\n|_|\n _|')
+        }
     }
-    if(args.includes('1')){  
-        numberArray.push('\n  |\n  |') 
-    }
-    if(args.includes('2')){
-        numberArray.push(' _\n _|\n|_ ')
-    }
-    if(args.includes('3')){
-        numberArray.push('_  \n_|\n_|')
-    }
-    if(args.includes('4')){
-        numberArray.push('  \n|_|\n  |')
-    }
-    if(args.includes('5')){
-        numberArray.push(' _\n|_ \n _|')
-    }
-    if(args.includes('6')){
-        numberArray.push(' _ \n|_ \n|_|')
-    }
-    if(args.includes('7')){
-        numberArray.push(' _\n  |\n  |')
-    }
-    if(args.includes('8')){
-        numberArray.push(' _\n|_|\n|_|')
-    }
-    if(args.includes('9')){
-        numberArray.push(' _\n|_|\n _|')
-    }
+    
     numberArray.forEach(element => {
         console.log(element);
     });
@@ -39,6 +42,5 @@ function convertToDigitalFormat(args){
 function main(){
     const args = process.argv.slice(2)
     convertToDigitalFormat(args);
-    
 }
 main()
